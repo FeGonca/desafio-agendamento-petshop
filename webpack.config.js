3510333e8204a28df070eccc9abe57370c501c0f
Felipe <felip.goncalves@hotmail.com>
2025-04-27 21:35:49 -0300

    Alterando para pasta src e copiando assets

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,52 @@
-const path = require("path")
-const HtmlWebpackPlugin = require("html-webpack-plugin")
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-    target: "web",
-    mode: "development",
+  target: "web",
+  mode: "development",
 
-    entry: path.resolve(__dirname, "src", "main.js"),
-    output: {
-        filename: "main.js",
-        path: path.resolve(__dirname, "dist")
+  entry: path.resolve(__dirname, "src", "main.js"),
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"),
     },
+    port: 3000,
+    open: true,
+    liveReload: true,
+  },
 
-    devServer: {
-        static: {
-            directory: path.join(__dirname, "dist")
-        },
-        port: 3000,
-        open: true,
-        liveReload: true
-    },
-
-    plugins: [
-        new HtmlWebpackPlugin(
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "index.html"),
+      favicon: path.resolve(
+        "src",
+        "assets",
+        "icons",
+        "Dog-Duotone--Streamline-Phosphor.svg"
+      ),
+    }),
+    new CopyWebpackPlugin({
+        patterns: [
             {
-                template: path.resolve(__dirname, "index.html"),
-                favicon: path.resolve("src", "assets", "icons", "Dog-Duotone--Streamline-Phosphor.svg"),
+                from: path.resolve(__dirname, "src", "assets"),
+                to: path.resolve(__dirname, "dist", "src", "assets"),
             }
-        )
-    ],
+        ]
+    })
+  ],
 
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: ["style-loader", "css-loader"]
-            },
-        ],
-    }
-}
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+};
